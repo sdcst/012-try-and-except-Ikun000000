4a46a317076e0a3c6c06951fbda4fd2d3503da17
Ikun000000 <159072923+Ikun000000@users.noreply.github.com>
2024-04-26 12:26:33 -0700

    finished

--- task2.py
+++ task2.py
@@ -19,3 +19,9 @@ The reciprocal of 3 is 0.3333333333333333
 Enter a number: 4
 The reciprocal of 4 is 0.25
 """
+number = input("Please enter in an integer value")
+try:
+    number = int(number)
+    print(1/number)
+except: 
+    print("Error")
